4216f115bbf513706b3f4d28bfbf6ce096ecc34e
Vish O <v.opatha@gmail.com>
2021-08-26 22:11:03 +0930

    Add seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE employee_db;
+
+-- INSERT INTO department (department_name) VALUES ("ICT"),("MARKETING"),("PRODUCTION"),("HR");
+
+-- INSERT INTO emp_role (title,salary,department_id) VALUES ("System Admin",100000,5),("Marketing Manager",120000,6),("Production Supervisor",72000,7),("Human Resource Manager",92000,8);
+
+INSERT INTO employee (first_name,last_name,role_id) VALUES ("Ara","Hetti",1),("Vish","Opatha",2),("xyz","abc",3);
