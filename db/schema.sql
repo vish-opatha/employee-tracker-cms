07eb7a8db325d3eaa036b0b6ddd17379135dffb7
Vish O <v.opatha@gmail.com>
2021-08-26 21:45:56 +0930

    Update database schema

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE emp_role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal,
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL, 
+  role_id INT,
+  mgr_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id)
+  REFERENCES emp_role(id)
+);
+
+ALTER TABLE employee ADD FOREIGN KEY(mgr_id) REFERENCES employee(id);
