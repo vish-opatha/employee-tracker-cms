8e6a0a8506e33a21cba81908fff8a30f5c602d96
Vish O <v.opatha@gmail.com>
2021-08-27 11:05:57 +0930

    Add seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
+  id INT AUTO_INCREMENT, 
+  department_name VARCHAR(30) NOT NULL, 
+  PRIMARY KEY (id));
+
+alter table department AUTO_INCREMENT=1;
 
 CREATE TABLE emp_role (
   id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary decimal,
+  salary DECIMAL,
   department_id INT,
-  PRIMARY KEY(id),
+  PRIMARY KEY (id),
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
+
+alter table emp_role AUTO_INCREMENT=1;
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT,
@@ -31,4 +34,5 @@ CREATE TABLE employee (
   REFERENCES emp_role(id)
 );
 
+alter table employee AUTO_INCREMENT=1; 
 ALTER TABLE employee ADD FOREIGN KEY(mgr_id) REFERENCES employee(id);
