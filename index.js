ad202aa1878ee99ceb7515a0602b49512b7128c1
Vish O <v.opatha@gmail.com>
2021-09-01 15:34:19 +0930

    Update index functions using try catch block

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-// Linking the userPrompt
+// Linking the required modules and packages
 const inquirer = require("inquirer");
 const cTable = require('console.table');
-const querryHandler = require("./utility/dbQuery");
-const mainChoices = [ 'View all Employees', 'View all Roles', 'View all Departments', 'Add Employee',
-                    'Update Employee Role', 'Add Role', 'Add Department', 'Exit'];
 const db = require('./db/connection');
 
+// Variable declaration
+const mainChoices = [ 'View all Employees', 'View all Roles', 'View all Departments', 'Add Employee',
+                    'Update Employee Role', 'Add Role', 'Add Department', 'Exit'];
 
 
-// Display main user prompts to the user
+// ####### Display main user prompts to the user #############
+// ############################################################
 async function displayMainPrompts() {
-    const data = await inquirer.prompt([
-    {
-        type: "list",
-        name: "taskList",
-        message: "What do you want to do?",
-        choices: mainChoices,
-    }]);
-    
-    
-    displaySecondaryPrompts(data.taskList);
+    try{
+        const data = await inquirer.prompt([
+        { type: "list", name: "taskList", message: "What do you want to do?", choices: mainChoices, }]);
+        
+        displaySecondaryPrompts(data.taskList);
+    }
 
+    catch(e){
+        console.error(e);
+    }
 };
 
-// Display secondary prompts to the user
+// ####### Display secondary prompts to the user ##############
+// ############################################################
 function displaySecondaryPrompts(selection){
     switch (selection) {
         case 'View all Employees':
-            showAllEmployees();        
+            viewAllEmployees();        
             break;
         
         case 'View all Roles':
@@ -36,65 +37,65 @@ function displaySecondaryPrompts(selection){
 
         case 'View all Departments':
             viewAllDepartments();
-            
             break;
 
         case 'Add Employee':
             addNewEmployee();
-        
             break;
 
         case 'Update Employee Role':
             updateEmpRole()
             break;
 
-
         case 'Add Role':
             addNewRole();
-            
             break;
 
         case 'Add Department':
             addNewDepartment();
             break;
-
-        case 'Exit':
-            console.log(`Exiting the application!`);
-            process.exit();
-        
-    
-            break;
     
         default:
             console.log(`Exiting the application!`);
+            db.end();
             process.exit();
             break;
     }
 }
 
+// ####### Functions to add employee, role and department #####
+// ############################################################
+
 async function addNewDepartment(){
     const data = await inquirer.prompt
-    ([{ type: "input", name: "departmentName", message: `What is the name of the department?`}]);
+    ([{ type: "input", name: "departmentName", message: `What is the name of the department?`,
+    validate(value) {
+        const pass = value.match(/^[a-zA-Z]+$/);
+        if (pass) {
+          return true;
+        }
+        return `Please enter department name`;
+       },}]);
 
     const sql = `INSERT INTO department (department_name) VALUES (?)`;
-
     const param = data.departmentName;
 
     db.query(sql, param, (err, result) => {
         if (err) {
-            console.log(err);
-          
+            console.log(err); 
         }
 
         console.log("New department is created!");
+        console.log();
         displayMainPrompts();
     });
 }
 
 async function addNewRole()
 {
+    try{
     const data = await inquirer.prompt([
-        { type: "input", name: "title", message: `What is the name of the role?`},
+        { type: "input", name: "title", message: `What is the name of the role?`,},
         { type: "input", name: "salary", message: `What is the salary of the role?`},
         { type: "input", name: "departmentId", message: `Which department does the role belong to?`},
     ]);
@@ -113,12 +114,15 @@ async function addNewRole()
         console.log("");
         displayMainPrompts();
     });
-    
-
+    }
+    catch(e){
+        console.error(e);
+    }
 }
 
 async function addNewEmployee()
 {
+    try{
     const data = await inquirer.prompt([
         { type: "input", name: "firstName", message: `What is the first name of the employee?`},
         { type: "input", name: "lastName", message: `What is the last name of the employee?`},
@@ -138,8 +142,14 @@ async function addNewEmployee()
         console.log("");
         displayMainPrompts();
     });
-
+    }
+    catch(e){
+        console.error(e);
+    }
 }
+
+// ####### Functions to update employee role ##################
+// ############################################################
 
 async function updateEmpRole(){
     const data = await inquirer.prompt([
@@ -173,7 +183,7 @@ async function updateEmpRole(){
 
 
 
-function showAllEmployees(){
+function viewAllEmployees(){
     console.log("Show employees");
      const sql = `SELECT employee.id AS 'Id', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', 
                 emp_role.title AS 'Position', department.department_name AS 'Department', 
